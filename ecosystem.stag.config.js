f944f7e5162f98b89d0f28befb6dffdea20bd498
duongha <haduong@beeketing.net>
2021-01-05 14:45:06 +0700

    add config pm2

---
+++ ecosystem.stag.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  apps: [
+    {
+      'name': 'asiabill',
+      'max_memory_restart': '1G',
+      'instances': '4',
+      'exec_mode': 'cluster',
+      'script': 'index.js',
+      'node_args': '--max_old_space_size=1024',
+      'log_date_format': 'YYYY-MM-DD HH:mm:ss',
+      'ignore_watch': [
+        'core/build/config.json',
+        'node_modules',
+      ],
+    },
+  ],
+};

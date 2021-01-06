ee6c39081b86cc032dcc79dfe7e5b559f0cf6916
duongha <haduong@beeketing.net>
2021-01-06 14:40:09 +0700

    add log debug

--- router.js
+++ router.js
@@ -9,8 +9,6 @@ const voidHandler = require('./src/handler/voidHandler');
 const gatewayCheckCredentialsHandler = require('./src/handler/gatewayCheckCredentialsHandler');
 const router = new express.Router();
 
-const redis = require('./src/lib/redis');
-
 router.post('/create-order', createOrderHandler);
 
 router.get('/provider-webhook', gatewayWebhookHandler);
@@ -18,12 +16,6 @@ router.get('/provider-webhook', gatewayWebhookHandler);
 router.post('/provider-confirm', gatewayConfirmHandler);
 
 router.post('/provider-check-credentials', gatewayCheckCredentialsHandler);
-
-router.get('/test', async (req, res) => {
-  const redirectOrder = await redis.get('test');
-  console.log(redirectOrder);
-  return res.render('redirect', JSON.parse(redirectOrder));
-});
 
 router.get('/transaction', getTransactionInfoHandler);
 

926c971c4df003bacff294b6d9a399d1827aecb6
duongha <haduong@beeketing.net>
2021-01-06 13:35:26 +0700

    remove config

--- ecosystem.dev.config.js
+++ ecosystem.dev.config.js
@@ -8,10 +8,6 @@ module.exports = {
       'script': 'index.js',
       'node_args': '--max_old_space_size=1024',
       'log_date_format': 'YYYY-MM-DD HH:mm:ss',
-      'ignore_watch': [
-        'core/build/config.json',
-        'node_modules',
-      ],
     },
   ],
 };

b60e0db4eb11503ced4efdeeae1a8e4f4e1629a6
Dzung Tran <hello@iamdzung.com>
2021-01-08 11:27:24 +0700

    Fix sandbox mode check credentials

--- src/entities/credential.js
+++ src/entities/credential.js
@@ -2,7 +2,7 @@
  * config credential
  @typedef PaymentGatewayCredential
  @type {Object}
- @property {boolean} isTestMode
+ @property {boolean} sandbox
  @property {string} merNo
  @property {string} gatewayNo
  @property {string} signKey
